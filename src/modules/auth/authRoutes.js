81db16230a1b34aa293dfcc034abaee8ef695b71
irvanmnwr <irvanmnwr@gmail.com>
2022-04-11 22:31:09 +0700

    testing

--- src/modules/auth/authRoutes.js
+++ src/modules/auth/authRoutes.js
@@ -6,7 +6,7 @@ const authController = require("./authController");
 
 Router.post("/login", authController.login);
 Router.post("/register", authController.register);
-Router.patch("/verification/:id", authController.verification);
+Router.post("/verification/:id", authController.verification);
 Router.patch("/refresh", authController.refresh);
 Router.post("/logout", authController.logout);
 
